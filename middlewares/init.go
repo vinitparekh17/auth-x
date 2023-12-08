e4b53ac8e18d52e0274ded894a9acaab22c9a374
vinitparekh17 <vinitparekh1742@gmail.com>
2023-12-08 23:16:19 +0530

    additional middlewares

--- middlewares/init.go
+++ middlewares/init.go
@@ -5,24 +5,35 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/vinitparekh17/project-x/config"
 	"github.com/vinitparekh17/project-x/utility"
 )
 
 func Init(e *echo.Echo) {
 	pwd, _ := os.Getwd()
-	file, err := os.OpenFile(pwd+"/logss/main.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	file, err := os.OpenFile(pwd+config.K.String("log_path"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	utility.ErrorHandler(err)
 	defer file.Close()
+
 	e.Use(middleware.BodyLimit("2M"))
-		
+
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format: "method=${method}, uri=${uri}, status=${status}, time=${latency_human}\n",
-		Output: file,
+		Format:           `method=${method}, uri=${uri}, status=${status}, time=${latency_human}`,
+		Output:           os.Stdout,
+		CustomTimeFormat: "2006-01-02 05:04:05",
 	}))
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: config.K.Strings("origin"),
+	}))
+
 	e.Use(middleware.Secure())
+
 	e.Use(middleware.GzipWithConfig(middleware.GzipConfig{
 		Level: 5,
 	}))
+
 	e.Use(middleware.RequestID())
+
 	e.Use(middleware.Recover())
 }
