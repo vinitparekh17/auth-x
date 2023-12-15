7167b8d7a2f5aa466ee70222262737d3cc3d4907
vinitparekh17 <vinitparekh1742@gmail.com>
2023-12-15 21:24:00 +0530

    port from env

--- apis/apis.go
+++ apis/apis.go
@@ -7,7 +7,8 @@ import (
 
 func Init(e *echo.Echo) {
 	user := e.Group("/user")
-	user.GET("/get", (&controllers.UserControllers{}).GetUsers)
+	user.POST("/signup", (&controllers.UserControllers{}).Signup)
+	user.POST("/login", (&controllers.UserControllers{}).Login)
 
 	health := e.Group("/health")
 	health.GET("/get", (&controllers.HealthController{}).GetHealth)
