a3d5de91dea6e3d9360e06cc95a23646c533d023
vinitparekh17 <vinitparekh1742@gmail.com>
2024-01-02 23:33:21 +0530

    redis configuration stable

--- redis/redis.go
+++ redis/redis.go
@@ -2,20 +2,42 @@ package redis
 
 import (
 	"context"
+	"errors"
 	"log/slog"
 
 	"github.com/redis/go-redis/v9"
+	"github.com/vinitparekh17/project-x/handler"
 )
 
 var ctx = context.Background()
+var rdb *redis.Client
 
 func Init() {
-	rdb := redis.NewClient(&redis.Options{
+	// Initialize redis client with default options
+	rdb = redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "secret",
 		DB:       0,
 	})
 
+	// Ping redis server to check connection
 	status := rdb.Ping(ctx)
 	slog.Info("Redis pinging... " + status.String())
+}
+
+func Set(key string, value string) {
+	err := rdb.Set(ctx, key, value, 0).Err()
+	handler.ErrorHandler(err)
+}
+
+func Get(key string) string {
+	val, err := rdb.Get(ctx, key).Result()
+	if err == redis.Nil {
+		handler.ErrorHandler(errors.New("key does not exist"))
+	} else if err != nil {
+		handler.ErrorHandler(err)
+	} else {
+		return val
+	}
+	return ""
 }
